bba43117aed2a5579ad548ff1cd7659a180f09df
astroboy20 <tolulopeakinkunmi7@gmail.com>
2025-11-05 14:01:03 +0100

    comment out outputFileTracingRoot in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const path = require("path");
 
 const nextConfig: NextConfig = {
   /* config options here */
-  outputFileTracingRoot: path.join(__dirname, "../../")
+  // outputFileTracingRoot: path.join(__dirname, "../../")
 };
 
 export default nextConfig;
